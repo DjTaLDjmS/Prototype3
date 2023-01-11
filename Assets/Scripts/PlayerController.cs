b865cc8b6a4093614ef2cdd5fce6c1d6928f60d8
DjTaLDjmS <sdemarco@hotmail.fr>
2023-01-12 00:01:46 +0100

    Lesson 3.1 - Jump Force - 6.Prevent player from double-jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     private float jumpforce;
     [SerializeField]
     private float gravityModifier;
+    [SerializeField]
+    private bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +21,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
-
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
