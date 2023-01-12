ddf9ffe763c91a6e271fe11226ffe0484b5ef059
DjTaLDjmS <sdemarco@hotmail.fr>
2023-01-12 21:34:48 +0100

    Lesson 3.3 - Don't Just Stand There - 6.Keep player from unconscious jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
             isOnGround = false;
